7a0c0bf8872393f8b4cf4dee1d5e50c42dca11e5
ForeseN <yagosik@gmail.com>
2023-02-19 22:35:08 +0200

    styled stayPreview

--- src/ts/pages/Home/cmps/StayPreview.tsx
+++ src/ts/pages/Home/cmps/StayPreview.tsx
@@ -1,3 +1,5 @@
+import { AiFillStar } from 'react-icons/ai'
+
 export default function StayPreview({ stay }: any) {
     return (
         <div>
@@ -5,9 +7,16 @@ export default function StayPreview({ stay }: any) {
                 <img src={stay.imgUrls[0]} alt='' />
                 {/* <ImgCarousel imgUrls={stay.imgUrls} /> */}
                 <div className='details'>
-                    <p className='address'>{stay.loc.address}</p>
+                    <div className='row'>
+                        <p className='address'>{stay.loc.address}</p>
+                        <div className='stars'>
+                            <AiFillStar color='black' />
+                            <p>5.0</p>
+                        </div>
+                    </div>
+
                     <p className='type'>{stay.type}</p>
-                    <p className="available">Jul 22 - 29</p>
+                    <p className='available'>Jul 22 - 29</p>
                     <p className='price'>
                         ${stay.price} <span className='prefix'>night</span>
                     </p>
