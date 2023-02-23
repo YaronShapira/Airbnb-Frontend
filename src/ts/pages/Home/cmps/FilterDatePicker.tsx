6adb7cff11ff659dbb5c2f324ffcafae8e6c8a89
ForeseN <yagosik@gmail.com>
2023-02-23 17:11:07 +0200

    added calendar

--- src/ts/pages/Home/cmps/FilterDatePicker.tsx
+++ src/ts/pages/Home/cmps/FilterDatePicker.tsx
@@ -1,3 +1,29 @@
+import { useState } from 'react'
+
+import { DateRangePicker } from 'react-date-range'
+import 'react-date-range/dist/styles.css' // main style file
+import 'react-date-range/dist/theme/default.css' // theme css file
+
 export default function FilterDatePicker() {
-    return <section className='filter-module filter-date-picker'>Hello from FilterDatePicker</section>
+    const [startDate, setStartDate] = useState(new Date())
+    const [endDate, setEndDate] = useState(new Date())
+    const selectionRange = {
+        startDate,
+        endDate,
+        key: 'selection',
+    }
+    function handleSelect(ranges: any) {
+        setStartDate(ranges.selection.startDate)
+        setEndDate(ranges.selection.endDate)
+    }
+    return (
+        <section className='filter-module filter-date-picker'>
+            <DateRangePicker
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={['#ff385c']}
+                onChange={handleSelect}
+            />
+        </section>
+    )
 }
