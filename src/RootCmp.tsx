b15054e463d0bdcec6b5b7254e87de76f4afb723
ForeseN <yagosik@gmail.com>
2023-02-25 17:19:21 +0200

    added routing

--- src/RootCmp.tsx
+++ src/RootCmp.tsx
@@ -1,11 +1,14 @@
 import './assets/style/main.scss'
 import Home from './ts/pages/Home/Home'
 import Navbar from './ts/pages/Home/cmps/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
     return (
         <div className='app'>
-            <Home />
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
         </div>
     )
 }
